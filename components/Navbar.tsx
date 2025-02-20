5d3be5d0fe0b73b3e63c3a9ff49cceeea43e8b0b
Inzamam <inzamamchowdhury20@gmail.com>
2025-02-21 00:01:37 +0600

    fix : User Image

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,11 +1,11 @@
-
 import Image from "next/image";
 import React from "react";
 import { Button } from "@/components/ui/button";
 import { auth, signIn } from "@/auth";
 
-const Navbar = async() => {
+const Navbar = async () => {
     const session = await auth();
+    
     return (
         <div className="flex justify-between items-center p-4">
             <div className="flex justify-center items-center gap-2">
@@ -15,20 +15,19 @@ const Navbar = async() => {
                     <p className="font-semibold">Generate. Scan. Connect.</p>
                 </div>
             </div>
-            {session ? ( 
-                <Image src={session?.user?.image || ""} alt="profile" height={50} width={50} className="rounded-full border-gray-500 border-2"/>
+            {session && session.user.image ? ( 
+                <Image src={session.user.image} alt="profile" height={50} width={50} className="rounded-full border-gray-500 border-2"/>
             ) 
             :
-             
             <form
                 action={async () => {
-                    "use server"
-                    await signIn("google")
+                    "use server";
+                    await signIn("google");
                 }}
             >
                 <Button type="submit">Signin with Google</Button>
             </form>
-}
+            }
         </div>
     );
 };
