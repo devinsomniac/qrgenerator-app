675b95154fe86df608b8a3af31c475193e15ce15
Inzamam <inzamamchowdhury20@gmail.com>
2025-02-21 21:03:08 +0600

    fix : next config

--- next.config.ts
+++ next.config.ts
@@ -1,3 +1,5 @@
+import { env } from "node:process";
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
@@ -8,6 +10,10 @@ const nextConfig = {
       },
     ],
   },
-};
+  env: {
+    DATABASE_URL: process.env.DATABASE_URL,
+    NEXTAUTH_URL: process.env.NEXTAUTH_URL,
+}
+}
 
 export default nextConfig;
